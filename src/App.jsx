a4ca3d06d355501a1ef273ea4468f67ff0c62684
DANUSHKA\danus <danushkasomasiri@gmail.com>
2025-04-15 09:49:23 +0200

    Modify header text

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 export const App = () => {
   return (
     <>
-      <h1>Portfolio</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, laborum! Maxime animi nostrum facilis distinctio neque labore consectetur beatae eum ipsum excepturi voluptatum, dicta repellendus incidunt fugiat, consequatur rem aperiam.</p>
+      <h1>Hi I am Danushka Somasiri</h1>
+      <p>Full Stack Developer</p>
+      <p>Danushka is a passionate developer with a strong foundation in front-end technologies and experience across the full stack. Comfortable with version control using Git and collaborative development workflows, they’ve also worked on dynamic back-end systems and database-driven applications. Their versatility and hands-on experience with modern development tools make them a valuable contributor to any tech team.</p>
     </>
   )
 }
