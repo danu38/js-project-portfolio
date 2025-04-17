c77f8d151a539e0df7fc954134792fff1bc3a707
DANUSHKA\danus <danushkasomasiri@gmail.com>
2025-04-17 11:11:05 +0200

    add media queries

--- src/components/Tech/Tech.jsx
+++ src/components/Tech/Tech.jsx
@@ -1,10 +1,12 @@
 import "./Tech.css"; 
+import techData from "../../data/techData.json"; // import the tech data from json file
+import React from "react"; // Import React library
 
 const Tech = () => {
     return (
         <div className="Tech_Container">
-            <div className="Tech_Container_Title">Tech</div>
-            <div className="Tech_Container_Desc">HTML, CSS, Flexbox, JavaScript, ES6, JSX, React, React Hooks, Node.js, Mongo DB, Web Accessibility, APIs, mob-programming, pair-programming, GitHub, PHP, MySQL, Linux Administration, Python, AI modeling  .</div>
+            <div className="Tech_Container_Title">{techData.title}</div>
+            <div className="Tech_Container_Desc">{techData.description}</div>
         </div>
     )
 }
