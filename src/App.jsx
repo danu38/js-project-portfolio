f5673f665e4655ac764ab3378da88a3b47017c43
DANUSHKA\danus <danushkasomasiri@gmail.com>
2025-04-17 09:12:52 +0200

    tech component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/header/Header';
+import Tech from './components/Tech/Tech';
 
 export const App = () => {
   return (
     <>
       <Header />
+      <Tech />
       <p></p>
     </>
   )
---
+++ src/components/Tech/Tech.jsx
@@ -0,0 +1,11 @@
+import "./Tech.css"; 
+
+const Tech = () => {
+    return (
+        <div className="Tech_Container">
+            <div className="Tech_Container_Title">Tech</div>
+            <div className="Tech_Container_Desc">HTML, CSS, Flexbox, JavaScript, ES6, JSX, React, React Hooks, Node.js, Mongo DB, Web Accessibility, APIs, mob-programming, pair-programming, GitHub, PHP, MySQL, Linux Administration, Python, AI modeling  .</div>
+        </div>
+    )
+}
+export default Tech;
