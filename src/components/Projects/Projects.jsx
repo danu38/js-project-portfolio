d6ca5cc47b59a468d43aaee8f8b6e78b35723a2d
DANUSHKA\danus <danushkasomasiri@gmail.com>
2025-04-22 00:30:16 +0200

    projects component styles fixed

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,7 +6,7 @@ import './Project.css';
 const Projects = () => {
   return (
     <section className="projects">
-      <h2 className="projects-title">Featured Projects</h2>console.log(projectsData)
+      <h2 className="projects-title">Featured Projects</h2>
       {projectsData.projects.map((project, index) => (
         <ProjectCard key={index} {...project} />
       ))}
